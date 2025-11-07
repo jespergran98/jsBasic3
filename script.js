b79eb54b368bdf2294849cfd9f9c6251daaf6f5c
jespergran98 <jesper25sfj@gmail.com>
2025-11-07 11:46:53 +0100

    la til tekst for oppgave 4

--- script.js
+++ script.js
@@ -177,3 +177,26 @@ function cleanText(inputArray) { // Funksjon som tar inn et array av strings som
 
 console.log(cleanText(pokemon)); // Skriv ut resultatet av cleanText-funksjonen
 
+
+// Oppgave 4:
+
+// Fullfør funksjonen nedenfor for å oppnå følgende:
+
+// Returner stringen som mottas i første parameter med følgende endringer:
+
+// Hver bokstav i stringen som matcher `charA` (andre parameteret) skal erstattes 
+// med `charB` (tredje parameteret), og VICE VERSA - det vil si at bokstaver som 
+// matcher `charA` skal byttes med `charB`, og bokstaver som matcher `charB` skal 
+// byttes med `charA`.
+
+// Eksempler:
+
+// doubleSwap("this is a string", "i", "s")
+// skal returnere "thsi si a itrsng"
+
+// doubleSwap("m#ybe #nother #ppro#ch is necess#ry", "#", "a")
+// skal returnere "maybe another approach is necessary"
+
+// doubleSwap("what is the point of this?", "o", "t")
+// skal returnere "whao is ohe ptino tf ohis?"
+
